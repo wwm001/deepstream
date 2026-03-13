33571c901142eaf75b756b123d1d43d6f62c6147
HOMMA Kazumichi <worldwidemusic001@gmail.com>
2026-03-13 16:22:43 +0900

    Style stream cards

--- src/components/StreamCard.tsx
+++ src/components/StreamCard.tsx
@@ -8,12 +8,34 @@ function StreamCard({ title, description }: StreamCardProps) {
     <article
       style={{
         padding: "16px",
-        borderRadius: "10px",
+        borderRadius: "12px",
         background: "#f3f4f6",
+        border: "1px solid #e5e7eb",
+        minHeight: "140px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
       }}
     >
-      <h3 style={{ marginTop: 0 }}>{title}</h3>
-      <p style={{ marginBottom: 0 }}>{description}</p>
+      <h3
+        style={{
+          marginTop: 0,
+          marginBottom: "12px",
+          fontSize: "16px",
+        }}
+      >
+        {title}
+      </h3>
+
+      <p
+        style={{
+          marginBottom: 0,
+          color: "#4b5563",
+          lineHeight: 1.6,
+        }}
+      >
+        {description}
+      </p>
     </article>
   );
 }
