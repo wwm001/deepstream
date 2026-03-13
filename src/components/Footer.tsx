e28aec815538cf82cbd5ab1df39299e7f7afe43a
HOMMA Kazumichi <worldwidemusic001@gmail.com>
2026-03-13 19:37:01 +0900

    Refine footer typography

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,27 @@ function Footer() {
     <footer
       style={{
         marginTop: "32px",
-        color: "#666",
         textAlign: "center",
       }}
     >
-      <small style={{ display: "block" }}>{appMeta.footerText}</small>
-      <small style={{ display: "block", marginTop: "6px", color: "#9ca3af" }}>
+      <small
+        style={{
+          display: "block",
+          color: "#4b5563",
+          fontWeight: 600,
+        }}
+      >
+        {appMeta.footerText}
+      </small>
+
+      <small
+        style={{
+          display: "block",
+          marginTop: "6px",
+          color: "#9ca3af",
+          lineHeight: 1.6,
+        }}
+      >
         {appMeta.statusText}
       </small>
     </footer>
